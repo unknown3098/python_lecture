fefa8c6426c52914724e730fa15375c73ed189a1
SongDongJun_0987 <songdongjun8284@gmail.com>
2026-04-05 21:42:14 +0900

    Add files via upload

---
+++ lecture_5_double_for.py
@@ -0,0 +1,27 @@
+
+for i in range(2, 10):
+    print(f"#  {i}단  #", end=" ")
+print()
+
+
+for k in range(1, 10):       
+    for i in range(2, 10):  
+        print(f"{i}X {k:>2}= {i*k:>2}", end=" ")
+    print() 
+
+
+
+    
+
+'''다른코드'''
+
+"""i, k =0, 0
+
+k = int(input("단을 입력하세요 : "))
+
+for i in range(1,10, 1):
+  print("%d * %d = 2d%" %)
+
+for i in range(2, 10):
+  print("# , i, ")
+"""
